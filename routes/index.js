54b03e97c81d1bebb85ddd753e9d1f1c2d10adb7
Saintogod <saintogod@gmail.com>
2017-11-23 22:41:11 +0800

    add endpoint for list all endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
+'use strict';
+
+var listEndpoints = require('express-list-endpoints');
+
 module.exports = function(app){
     require('./userRouter')(app);
     require('./foodRouter')(app);
     require('./orderRouter')(app);
     require('./containerRouter')(app);
+
+    app.route('/')
+        .get((req, res, next) => res.send(listEndpoints(app)));
 };
